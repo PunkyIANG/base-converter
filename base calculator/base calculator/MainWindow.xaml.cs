e23569b314d1f6b3bc59a7adc4c77e527687da51
zapto123 <tsoorckanukris@gmail.com>
2019-11-13 16:23:30 +0200

    first commit
    copied the main xaml and c# scripts, removed references for external classes

---
+++ base calculator/base calculator/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace base_calculator
+{
+    public partial class MainWindow
+    {
+        TextBox focusedTxtbox;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void BtnClick(object sender, RoutedEventArgs e)
+        {
+            if (focusedTxtbox != null && ((Button)sender).Tag != null)
+            {
+                focusedTxtbox.Text += ((Button)sender).Tag;
+                focusedTxtbox.Select(focusedTxtbox.Text.Length, 0);
+            }
+        }
+
+        private void TxtBoxGotFocus(object sender, RoutedEventArgs e)
+        {
+            focusedTxtbox = (TextBox)sender;
+        }
+
+        private void Backspace(object sender, RoutedEventArgs e)
+        {
+            if (focusedTxtbox != null && focusedTxtbox.Text.Length != 0)
+            {
+                focusedTxtbox.Text = focusedTxtbox.Text.Remove(focusedTxtbox.CaretIndex - 1, 1);
+                focusedTxtbox.Select(focusedTxtbox.Text.Length, 0);
+            }
+        }
+    }
+}
